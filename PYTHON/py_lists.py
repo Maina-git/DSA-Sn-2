375368a77c38288df43786433c4f19b531143d59
Maina-git <francismm2023@gmail.com>
2026-06-11 20:12:36 +0300

    added python to my code

---
+++ PYTHON/py_lists.py
@@ -0,0 +1,32 @@
+#List 
+
+# it  is  like  a tupple  in typescript  you store data of  different types in an array
+
+ages = [19, 26, 29]
+
+print(ages)
+
+student = ["jack", 32, "Computer Science", [2, 4]]
+print(student)
+
+empty_list = []
+print(empty_list)
+
+
+languages = [ "Python", "Swift", "C++"]
+#access the  items in the  list just like in the arrays
+print('languages[2] =', languages[2])
+#negative indexing
+print('languages[-3]', languages[-3])
+
+#slicing an array
+my_list = ["p", "r", "o", "g", "r", "a", "m"]
+#means  from index 2 to 4 5 is not  included
+print("my_list", my_list[2: 5])
+print("my_list", my_list[2:-2])
+print("my_list", my_list[0: 3])
+
+# to be continued
+
+
+
